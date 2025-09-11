184798859d66d8f47ca47f88acaf18f4340cc34d
Sisir8642 <baburambista01@gmail.com>
2025-09-12 01:04:27 +0545

    fix:image problem

--- src/components/productCard/page.tsx
+++ src/components/productCard/page.tsx
@@ -13,32 +13,33 @@ interface Props {
 const ProductCard: React.FC<Props> = ({ product }) => {
   return (
     <div className="w-full">
-   
-     <Card className='hover:shadow-lg transition-all duration-300 transform hover:scale-105' >
-      <CardHeader>
-        <CardTitle>{product.name}</CardTitle>
-        <CardDescription>{product.description}</CardDescription>
-        <CardAction>
+
+      <Card className='hover:shadow-lg transition-all duration-300 transform hover:scale-105' >
+        <CardHeader>
+          <CardTitle>{product.name}</CardTitle>
+          <CardDescription>{product.description}</CardDescription>
+          <CardAction>
             <CartSheet triggerProduct={product} />
-        </CardAction>
-      </CardHeader>
+          </CardAction>
+        </CardHeader>
 
-      <CardContent>
-        <Image
-          src={getImage(product.name)}
-          alt={product.name}
-          className="w-full h-48 object-cover rounded"
-          width={400}  
-    height={250} 
-        />
-      </CardContent>
+        <CardContent>
+          <Image
+            src={getImage(product.name)}
+            alt={product.name}
+            className="w-full h-48 object-cover rounded"
+            width={400}
+            height={250}
+            unoptimized
+          />
+        </CardContent>
 
-      <CardFooter className='flex flex-col items-start space-y-1'>
-        <p className='text-yellow-300 text-2xl'>Rs. {product.price}</p>
-        <p className='line-through text-gray-400'>Rs. {Number(product.price) * 1.5}</p>
-        
-      </CardFooter>
-    </Card>
+        <CardFooter className='flex flex-col items-start space-y-1'>
+          <p className='text-yellow-300 text-2xl'>Rs. {product.price}</p>
+          <p className='line-through text-gray-400'>Rs. {Number(product.price) * 1.5}</p>
+
+        </CardFooter>
+      </Card>
 
     </div>
   );
