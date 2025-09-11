f9fe83f4bbc242b5068fe6de9380d6cde0cab0ef
Sisir8642 <baburambista01@gmail.com>
2025-09-12 01:35:26 +0545

    Fix folder case: Images → images

--- src/components/ImagePath/page.tsx
+++ src/components/ImagePath/page.tsx
@@ -1,16 +1,26 @@
 export const getImage = (name: string) => {
   switch(name.toLowerCase()) {
-    case 'bike':
+    case 'bread':
       return '/images/bread.png';
-    case 'technology':
+    case 'macbook':
       return '/images/macbook.png';
     case 'rice':
       return '/images/rice.png';
-    case 'hello':
-      return '/images/image2.png';
     case 's21':
       return '/images/s21.png';
-    default:
+    case 'image':
       return '/images/image.png';
+    case 'image2':
+      return '/images/image2.png';
+    case 'image3':
+      return '/images/image3.png';
+    case 'image4':
+      return '/images/image4.png';
+    case 'image5':
+      return '/images/image5.png';
+    case 'image6':
+      return '/images/image6.png';
+    default:
+      return '/images/image.png'; 
   }
 }
