6a3b812e26cc23fa2e969b6f3820dd388727cd3e
Sisir8642 <baburambista01@gmail.com>
2025-09-12 01:01:05 +0545

    fix: errors in the images

--- src/components/ImagePath/page.tsx
+++ src/components/ImagePath/page.tsx
@@ -1,17 +1,16 @@
-export const getImage =(name:string) =>{
-    switch(name.toLowerCase()){
-        case 'bread':
-            return '/images/bread.png'
-        case 'macbook':
-            return '/images/macbook.png'
-        case 'rice':
-            return '/images/rice.png'
-        case 'hello':
-            return 'images/image copy 2.png'
-            
-        case 's21':
-            return '/images/s21.png'
-        default:
-            return '/images/image.png'
-    }
+export const getImage = (name: string) => {
+  switch(name.toLowerCase()) {
+    case 'bike':
+      return '/images/bread.png';
+    case 'technology':
+      return '/images/macbook.png';
+    case 'rice':
+      return '/images/rice.png';
+    case 'hello':
+      return '/images/image_copy_2.png';
+    case 's21':
+      return '/images/s21.png';
+    default:
+      return '/images/image.png';
+  }
 }
